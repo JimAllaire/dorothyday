e46bf69fc62c9400068e9cc29e87cab9ba0c6157
Jim Allaire <james.allaire@gmail.com>
2013-08-28 12:15:13 -0400

    8/28/13
    create theme on hospitality

--- _books/make-theme-books.sh
+++ _books/make-theme-books.sh
@@ -4,7 +4,7 @@
 ./make-theme-book.sh "On Work" 154 227 229 431 452
 ./make-theme-book.sh "On War and Peace" 215 306 360 868 390 250
 ./make-theme-book.sh "On Peter Maurin" 148 151 152 155 495 176 914 256
-
+./make-theme-book.sh "On Hospitality" 435 416
 
 
 
