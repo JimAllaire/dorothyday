446cd35661a1543ba06bde9338390f904999aadf
Jim Allaire <james.allaire@gmail.com>
2013-09-15 12:14:23 -0400

    9/15/13
    Aims and purposes theme

--- _books/make-theme-books.sh
+++ _books/make-theme-books.sh
@@ -1,11 +1,11 @@
 
 #!/bin/bash
 
-./make-theme-book.sh "On Work" 154 227 229 431 452
+#./make-theme-book.sh "On Work" 154 227 229 431 452
 #./make-theme-book.sh "On War and Peace" 215 306 360 868 390 250
 #./make-theme-book.sh "On Peter Maurin" 148 151 152 155 495 176 914 256
 #./make-theme-book.sh "On Hospitality" 435 416 358 308 851
-#./make-theme-book.sh "On Aims and Purposes" 12 266 182 919 155 158 170 177 529
+./make-theme-book.sh "On Aims and Purposes" 12 266 182 919 155 456 158 170 177 529
 
 
 
